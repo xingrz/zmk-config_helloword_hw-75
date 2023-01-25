230d446dd558ae50c20dedfb9c102b021227f1df
XiNGRZ <hi@xingrz.me>
2023-01-25 11:30:44 +0800

    knob_app: Do calibration at dedicated thread
    So it won't hang the first draw of the screen

--- config/boards/arm/hw75_dynamic/app/knob_app.c
+++ config/boards/arm/hw75_dynamic/app/knob_app.c
@@ -19,10 +19,34 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include "knob_app.h"
 #include "indicator_app.h"
 
+#define KNOB_APP_THREAD_STACK_SIZE 1024
+#define KNOB_APP_THREAD_PRIORITY 10
+
 static const struct device *knob;
 static const struct device *motor;
 
 static bool motor_demo = false;
+
+K_THREAD_STACK_DEFINE(knob_work_stack_area, KNOB_APP_THREAD_STACK_SIZE);
+static struct k_work_q knob_work_q;
+
+static void knob_app_calibrate(struct k_work *work)
+{
+	indicator_set_bits(INDICATOR_KNOB_CALIBRATING);
+
+	int ret = motor_calibrate_auto(motor);
+	if (ret != 0) {
+		LOG_ERR("Motor is not calibrated");
+		return;
+	}
+
+	knob_set_mode(knob, KNOB_ENCODER);
+	knob_set_encoder_report(knob, true);
+
+	indicator_clear_bits(INDICATOR_KNOB_CALIBRATING);
+}
+
+K_WORK_DEFINE(calibrate_work, knob_app_calibrate);
 
 static int knob_app_init(const struct device *dev)
 {
@@ -40,24 +64,21 @@ static int knob_app_init(const struct device *dev)
 		return -ENODEV;
 	}
 
-	indicator_set_bits(INDICATOR_KNOB_CALIBRATING);
+	k_work_queue_start(&knob_work_q, knob_work_stack_area,
+			   K_THREAD_STACK_SIZEOF(knob_work_stack_area), KNOB_APP_THREAD_PRIORITY,
+			   NULL);
 
-	int ret = motor_calibrate_auto(motor);
-	if (ret != 0) {
-		LOG_ERR("Motor is not calibrated");
-		return -EIO;
-	}
-
-	knob_set_mode(knob, KNOB_ENCODER);
-	knob_set_encoder_report(knob, true);
-
-	indicator_clear_bits(INDICATOR_KNOB_CALIBRATING);
+	k_work_submit_to_queue(&knob_work_q, &calibrate_work);
 
 	return 0;
 }
 
 static void knob_app_update_motor_state(void)
 {
+	if (!knob || !motor) {
+		return;
+	}
+
 	if (motor_is_calibrated(motor) && !motor_demo) {
 		knob_set_enable(knob, zmk_activity_get_state() == ZMK_ACTIVITY_ACTIVE);
 	}
