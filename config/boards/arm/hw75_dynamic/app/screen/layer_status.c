14ffcfab2795f71379d5418d51d84760d39ba35e
XiNGRZ <hi@xingrz.me>
2023-01-26 15:18:06 +0800

    screen: Show name of active layer on top

--- config/boards/arm/hw75_dynamic/app/screen/layer_status.c
+++ config/boards/arm/hw75_dynamic/app/screen/layer_status.c
@@ -29,13 +29,25 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #define LAYER_LABEL(node) COND_CODE_0(DT_NODE_HAS_PROP(node, label), (NULL), (DT_LABEL(node))),
 
+LV_FONT_DECLARE(icons_19);
+
+#define UI_SYMBOL_VOLUME "\xEF\x80\xA8"
+#define UI_SYMBOL_BRIGHTNESS "\xEF\x86\x85"
+#define UI_SYMBOL_SCROLL "\xEF\x83\x9C"
+
+static const char *layer_icons[KEYMAP_LAYERS_NUM] = {
+	UI_SYMBOL_VOLUME,
+	UI_SYMBOL_BRIGHTNESS,
+	UI_SYMBOL_SCROLL,
+};
+
 static const char *layer_names[KEYMAP_LAYERS_NUM] = { DT_FOREACH_CHILD(KEYMAP_NODE, LAYER_LABEL) };
 
+static lv_obj_t *layer_name;
 static lv_obj_t *layer_list;
 static lv_obj_t *layer_items[KEYMAP_LAYERS_NUM] = {};
 
-LV_FONT_DECLARE(icons_19);
-
+static lv_style_t st_name;
 static lv_style_t st_list;
 static lv_style_t st_item;
 
@@ -47,6 +59,7 @@ static void update_layer_status(struct layer_status_state *state)
 {
 	lv_list_focus_btn(layer_list, layer_items[state->index]);
 	lv_list_focus(layer_items[state->index], LV_ANIM_ON);
+	lv_label_set_text(layer_name, layer_names[state->index]);
 }
 
 static void layer_status_update_cb(struct layer_status_state state)
@@ -77,10 +90,13 @@ static void layer_button_selected(lv_obj_t *obj, lv_event_t event)
 
 int layer_status_init(lv_obj_t *parent, lv_group_t *group)
 {
+	lv_style_init(&st_name);
+	lv_style_set_pad_ver(&st_name, LV_STATE_DEFAULT, 4);
+	lv_style_set_pad_hor(&st_name, LV_STATE_DEFAULT, 0);
+
 	lv_style_init(&st_list);
 	lv_style_set_radius(&st_list, LV_STATE_DEFAULT, 0);
-	lv_style_set_pad_ver(&st_list, LV_STATE_DEFAULT, 0);
-	lv_style_set_pad_hor(&st_list, LV_STATE_DEFAULT, 0);
+	lv_style_set_pad_all(&st_list, LV_STATE_DEFAULT, 0);
 	lv_style_set_margin_all(&st_list, LV_STATE_DEFAULT, 0);
 
 	lv_style_init(&st_item);
@@ -95,13 +111,22 @@ int layer_status_init(lv_obj_t *parent, lv_group_t *group)
 	lv_style_set_text_color(&st_item, LV_STATE_FOCUSED, LV_COLOR_WHITE);
 	lv_style_set_text_font(&st_item, LV_STATE_DEFAULT, &icons_19);
 
-	lv_obj_t *list = layer_list = lv_list_create(parent, NULL);
+	lv_obj_t *cont = lv_cont_create(parent, NULL);
+	lv_cont_set_layout(cont, LV_LAYOUT_COLUMN_MID);
+	lv_cont_set_fit(cont, LV_FIT_NONE);
+	lv_obj_set_size(cont, SCREEN_W, SCREEN_H);
+
+	lv_obj_t *name = layer_name = lv_label_create(cont, NULL);
+	lv_label_set_align(name, LV_LABEL_ALIGN_CENTER);
+	lv_obj_add_style(name, LV_LABEL_PART_MAIN, &st_name);
+
+	lv_obj_t *list = layer_list = lv_list_create(cont, NULL);
 	lv_list_set_layout(list, LV_LAYOUT_COLUMN_MID);
 	lv_obj_add_style(list, LV_OBJ_PART_MAIN, &st_list);
-	lv_obj_set_size(list, SCREEN_W, SCREEN_H);
+	lv_obj_set_size(list, SCREEN_W, SCREEN_H - 20);
 
 	for (int i = 0; i < KEYMAP_LAYERS_NUM; i++) {
-		lv_obj_t *btn = layer_items[i] = lv_list_add_btn(list, NULL, layer_names[i]);
+		lv_obj_t *btn = layer_items[i] = lv_list_add_btn(list, NULL, layer_icons[i]);
 		lv_obj_add_style(btn, LV_BTN_PART_MAIN, &st_item);
 		lv_btn_set_fit(btn, LV_FIT_NONE);
 		lv_obj_set_size(btn, ITEM_SIZE, ITEM_SIZE);
